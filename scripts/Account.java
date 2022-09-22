a7d1196a0c686fdce021afaa2f8159455a675356
camwlsn <cam.wilson10@gmail.com>
2022-09-22 13:33:06 -0700

    Deposit feature has been completed

--- scripts/Account.java
+++ scripts/Account.java
@@ -1,4 +1,26 @@
 
-public class Account {
+public class Account extends bankMain {
 
+	double balance = 0;
+	String accountName;
+	String accountId;
+	
+	Account(String name, String id, double balance){
+		accountName = name;
+		accountId = id;
+		this.balance = balance;
+		
+	}
+	void deposit(double amount) {
+		if(amount != 0) {
+			balance = balance + amount;
+			System.out.println("Successfully deposited: $"+amount);
+			System.out.println("Your balance is now: $"+balance);
+			System.out.println("**************************************");
+		} else {
+			System.out.println("You cannot make a deposit of $0");
+			System.out.println("**************************************");
+		}
+		
+	}
 }
--- scripts/bankMain.java
+++ scripts/bankMain.java
@@ -4,6 +4,15 @@ public class bankMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Account user1 = new Account("Chadwick P.", "BA001", 6900);
+		Account user2 = new Account("Mike Wazowski", "BA002", 0);
+		
+		
+		user1.deposit(100000);
+		//user1.withdraw(6900);
+		
+		user2.deposit(400);
+		//user2.withdraw(401);
 	}
 
 }
