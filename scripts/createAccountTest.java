b4fa885e5a926472d435d19f97f3eec070b3529a
Karlen Speiser <karlen@Karlens-MacBook-Air.local>
2022-09-23 13:33:56 -0700

    add comments to create account test

--- scripts/createAccountTest.java
+++ scripts/createAccountTest.java
@@ -7,10 +7,14 @@ public class createAccountTest extends bankMain{
 
 	@Test
 	public void test(){
+		//get new account details
 		String userName = "steve",userId = "BA15";
 		double userBal = 5000;
+		//make account object
 		Account test = new Account(userName,userId,userBal);
+		//make account using bankMain method
 		Account user = bankMain.createAccount(userName, userBal, 15);
+		//test values verifying objection creation
 		assertTrue(test.accountName==user.accountName);
 		assertTrue(test.accountId==user.accountId);
 		assertTrue(test.balance==user.balance);
