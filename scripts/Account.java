73358de5cd555ccfb35e5ce38bb55380ccd0ec37
Karlen Speiser <karlen@Karlens-MacBook-Air.local>
2022-09-23 13:45:04 -0700

    small fixes and comments

--- scripts/Account.java
+++ scripts/Account.java
@@ -11,30 +11,37 @@ public class Account extends bankMain {
 		this.balance = balance;
 		
 	}
+	//deposit function, checks that deposit > 0
 	void deposit(double amount) {
 		if(amount != 0) {
 			balance = balance + amount;
+			System.out.println("**************************************");
 			System.out.println("Successfully deposited: $"+amount);
 			System.out.println("Your balance is now: $"+balance);
 			System.out.println("**************************************");
 		} else {
+			System.out.println("**************************************");
 			System.out.println("You cannot make a deposit of $0");
 			System.out.println("**************************************");
 		}
 	}
+	//withdraw function, check for sufficient funds in balance
 	void withdraw(double amount) {
 		if(amount != 0) {
 			if(amount > balance) {
+				System.out.println("**************************************");
 				System.out.println("You do not have the sufficient funds to withdraw: $"+amount);
 				System.out.println("Your balance: $"+balance);
 				System.out.println("**************************************");
 			} else {
 				balance = balance - amount;
+				System.out.println("**************************************");
 				System.out.println("Successfully withdrawn: $"+amount);
 				System.out.println("Remaining balance: $"+balance);
 				System.out.println("**************************************");
 			}
 		} else {
+			System.out.println("**************************************");
 			System.out.println("You cannot withdraw $0");
 			System.out.println("**************************************");
 		}
